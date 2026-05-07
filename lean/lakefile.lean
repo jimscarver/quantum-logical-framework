d207ae893c79bb42065637d88c92349764f309ba
Jim Whitescarver <jimscarver@gmail.com>
2026-05-07 08:56:45 -0400

    Initialize quantum logical framework with libraries

--- lean/lakefile.lean
+++ lean/lakefile.lean
@@ -1,2 +1,16 @@
+name «quantum-logical-framework»
+version "0.1.0"
+
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
+
+@[default_target]
+lean_lib «QLFAxioms» {
+  srcDir := "lean"
+}
+lean_lib «QLFQuCalc» {
+  srcDir := "lean"
+}
+lean_lib «QLFRiemann» {
+  srcDir := "lean"
+}
