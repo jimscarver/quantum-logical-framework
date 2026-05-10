c90ffcc49e0794ac7ad9ee115fecd3229befed62
Jim Whitescarver <jimscarver@gmail.com>
2026-05-10 09:45:31 -0400

    Refactor Lakefile to consolidate libraries

--- lakefile.lean
+++ lakefile.lean
@@ -1,27 +1,7 @@
 import Lake
 open Lake DSL
 
-package «quantum_logical_framework»
+package .
 
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
-
-@[default_target]
-lean_lib «QLF_Axioms» where
-  srcDir := "lean"
-
-@[default_target]
-lean_lib «QLF_QuCalc» where
-  srcDir := "lean"
-
-@[default_target]
-lean_lib «QLF_Riemann» where
-  srcDir := "lean"
-
-@[default_target]
-lean_lib «QLF_Universality» where
-  srcDir := "lean"
-
-@[default_target]
-lean_lib «QLF_Critical_Line» where
+lean_lib QuantumLogicalFramework where
   srcDir := "lean"
